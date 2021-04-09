f0b57c7d24b8db88b74a7b29f9e683e8354a8875
Melted-Cheesefondue <80699765+Melted-Cheesefondue@users.noreply.github.com>
2021-04-09 11:09:28 +0100

    Add files via upload

--- BanditsCore.py
+++ BanditsCore.py
@@ -11,13 +11,13 @@ import copy
 orientations = ['N','E','S','W'] #the four orientations/directions: north, east, south, and west.
 
 bandit_positions = {'W':[0,30], 'S':[30,0], 'N':[30,60], 'E':[60,30]} #position of the four bandit machines, labelled W (west), S (south), etc...
-square_positions = []
+human_positions = []
 for j in range(1,6): #adding the remaining positions for the grid
-    square_positions.append([j*10,30])
+    human_positions.append([j*10,30])
     if j != 3: #don't add the middle position twice
-        square_positions.append([30,j*10])
+        human_positions.append([30,j*10])
 
-human_positions = square_positions[:] #the human can occupy all of these positions
+square_positions = human_positions[:] #the human can occupy all of these positions
 square_positions = square_positions + list(bandit_positions.values()) #add the bandit positions to the squares; the human cannot occupy these positions as the bandits are already there
 
 deltas = {'N':[0,10], 'E':[10,0], 'S':[0,-10], 'W':[-10,0]} #moving north means increasing y by 10 and leaving x alone; and so on for the other three directions
